f15e8221bacdaf18f7210e294aae2cc87b821b1d
Stefan Harrington-Palmer <stef@bear.army>
2018-11-14 10:27:51 +0000

    Big changes, also moving repo

--- weather.ps1
+++ weather.ps1
@@ -1,28 +1,29 @@
-#######################
-#######################
-#######################
+## Run at 1am
 
-# Don't change options in this file, instead edit the .ini file
-Get-Content ".\weather.ini" | foreach-object -begin {$h = @{}} -process { $k = [regex]::split($_, '='); if (($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+$config = Get-Content -Path .\config.json -Raw | ConvertFrom-Json
 
-# Set variables pulled in from .ini file
-#x# Maybe this can be pulled in with a loop in future? seems a lot of code.
-$api_key = $h.Get_Item("api_key")
-$verbose = $h.Get_Item("verbose")
-$temp_low_sketchy = $h.Get_Item("temp_low_sketchy")
-$temp_low_good = $h.Get_Item("temp_low_good")
-$temp_sweetspot = $h.Get_Item("temp_sweetspot")
-$temp_sweetspot_range = $h.Get_Item("temp_sweetspot_range")
-$temp_high_good = $h.Get_Item("temp_high_good")
-$temp_high_sketchy = $h.Get_Item("temp_high_sketchy")
-$windspeed_sketchy = $h.Get_Item("windspeed_sketchy")
-$windspeed_bad = $h.Get_Item("windspeed_bad")
-$rain_sketchy = $h.Get_Item("rain_sketchy")
-$rain_bad = $h.Get_Item("rain_bad")
+$Weather_API_Key = $config.weather.API_Key
+#$api_key = $Weather_API_Key #Hashed for testing removal
 
-#x# Put something in here to chose the json id for time slot. 0 = nowest, 1=+3 hrs, 2=+6hrs, etc
+$temp_low_sketchy = $config.weather.temp_low_sketchy
+$temp_low_good = $config.weather.temp_low_good
+$temp_sweetspot = $config.weather.temp_sweetspot
+$temp_sweetspot_range = $config.weather.temp_sweetspot_range
+$temp_high_good = $config.weather.temp_high_good
+$temp_high_sketchy = $config.weather.temp_high_sketchy
+$windspeed_sketchy = $config.weather.windspeed_sketchy
+$windspeed_bad = $config.weather.windspeed_bad
+$rain_sketchy = $config.weather.rain_sketchy
+$rain_bad = $config.weather.rain_bad
+$cycling_out_time = $config.weather.cycling_out_time
+$cycling_home_time = $config.weather.cycling_home_time
+$cycling_out_time       = (get-date -UFormat %Y-%m-%d) + " " + $cycling_out_time
+$cycling_home_time      = (get-date -UFormat %Y-%m-%d) + " " + $cycling_home_time
+#$cycling_out_time = "2018-11-12 " + $cycling_out_time      #For Development, comment above out, uncomment these
+#$cycling_home_time = "2018-11-12 " + $cycling_home_time    #For Development, comment above out, uncomment these
 
-#x# Need to find somewhere tidier to put this.
+write-verbose "[SETUP] Using ($cycling_out_time) for outwards and ($cycling_home_time) for homewards"
+
 $locations = @(
     [PSCustomObject]@{Location = "Start" ; CityID = "2657356"; Name = "Amersham"}
     [PSCustomObject]@{Location = "Mid1" ; CityID = "2639381"; Name = "Rickmansworth"}
@@ -31,10 +32,11 @@ $locations = @(
     [PSCustomObject]@{Location = "Finish" ; CityID = "2647262"; Name = "Hayes"}
 )
 
-#x# these are only feel because I keep re-running in the same session
-$good = 0
-$sketchy = 0
-$bad = 0
+if ($good -or $sketchy -or $bad) {
+    Clear-Variable -Force good
+    Clear-Variable -Force sketchy
+    Clear-Variable -Force bad
+}
 
 #Fault Checking. 
 # Are the cities correct?
@@ -47,66 +49,70 @@ $locations | ForEach-Object {
     $CityID = $_.CityID
     $CityName = $_.Name
     $Location = $_.Location
+    write-verbose "[Query] against CityID: $CityID, CityName: $CityName, Location: $Location."
+    $response = (Invoke-RestMethod -Uri ("http://api.openweathermap.org/data/2.5/forecast?id=" + $CityID + "&APPID=" + $Weather_API_Key + "&units=metric")).list[1]
+    #$response-home = (Invoke-RestMethod -Uri ("http://api.openweathermap.org/data/2.5/forecast?id=" + $CityID + "&APPID=" + $Weather_API_Key + "&units=metric")).list[4]
+    #$out = $y
+    #$home = $z
 
-    $y = (Invoke-RestMethod -Uri ("http://api.openweathermap.org/data/2.5/forecast?id=" + $CityID + "&APPID=" + $api_key + "&units=metric")).list[0]
+    $working_temp = ($response).main.temp_min
+    $windspeed = ($response).wind.speed
+    $rain_3hrs = ($response).rain."3h"
 
-    #temperature is in degrees C
-    $working_temp = ($y).main.temp_min
-    #wind is in metres per second.
-    $windspeed = ($y).wind.speed
-    #$winddirection=($y).wind.deg
-    #rain volume is in mm per last 3 hours
-    $rain_3hrs = ($y).rain."3h"
-    if (!$rain_3hrs) { $rain_3hrs = "0" } 
+    write-verbose "[Results] gave me TEMP: $working_temp, WIND; $windspeed and RAIN: $rain_3hrs"
+    $rain_3hrs = [math]::Round($rain_3hrs, 2)
     if ($error_reason) { clear-variable -Name "error_reason" }
+    write-verbose "[Results] Adjusted gave me TEMP: $working_temp, WIND; $windspeed and RAIN: $rain_3hrs"
+
 
     if ($working_temp -gt "$temp_low_good" -AND $working_temp -lt "$temp_high_good") {
-        "TEMP: $Location is good ($working_temp °C)"
-     #   $goodness_temp
+        Write-Verbose "[TEMP]: $CityName ($Location) is Good ($working_temp C)"
         $good++
     }
     elseif ($working_temp -gt "$temp_low_sketchy" -AND $working_temp -lt "$temp_high_sketchy") {
-        "TEMP: $Location Is sketchy ($working_temp °C)"
+        Write-Verbose "[TEMP]: $CityName ($Location) is Sketchy ($working_temp C)"
         $sketchy++
     }
     else {
-        "TEMP: $Location Is bad ($working_temp °C)"
+        Write-Verbose "[TEMP]: $CityName ($Location) is Bad ($working_temp C)"
         $error_reason = "TEMP: Failed because of Low Temperature $working_temp at $Location ($CityName)"
         $bad++
     }
 
-
-
+    Write-Verbose "[Progress] Good: $good, Sketchy: $Sketchy, Bad: $bad"
 
     if ($windspeed -lt "$windspeed_sketchy") {
-        "WIND: $Location is good ($windspeed M/s)"
+        Write-Verbose "[WIND]: $CityName ($Location) is Good ($windspeed M/s)"
         $good++
     }
     elseif ($windspeed -gt "$temp_low_sketchy" -AND $windspeed -lt "$windspeed_bad") {
-        "WIND: $Location Is sketchy ($windspeed M/s)"
+        Write-Verbose "[WIND]: $CityName ($Location) is Sketchy ($windspeed M/s)"
         $sketchy++
     }
     else {
-        "WIND: $Location Is bad ($windspeed M/s)"
+        Write-Verbose "[WIND]: $CityName ($Location) is Bad ($windspeed M/s)"
         $error_reason = "WIND: Failed because Windspeed over $windspeed M/s at $Location ($CityName)"
         $bad++
     }
 
+    Write-Verbose "[Progress] Good: $good, Sketchy: $Sketchy, Bad: $bad"
 
     if ($rain_3hrs -lt "$rain_sketchy") {
-        "RAIN: $Location is good ($rain_3hrs mm/3Hrs)"
+        Write-Verbose "[RAIN]: $CityName ($Location) is Good ($rain_3hrs mm/3hrs)"
         $good++
     }
     elseif ($rain_3hrs -lt "$rain_bad") {
-        "RAIN: $Location Is sketchy ($rain_3hrs mm/3Hrs)"
+        Write-Verbose "[RAIN]: $CityName ($Location) is Sketchy ($rain_3hrs mm/3hrs)"
         $sketchy++
     }
     else {
-        "RAIN: $Location Is bad ($rain_3hrs M/s)"
+        #"RAIN: $Location Is bad ($rain_3hrs M/s)"
+        Write-Verbose "[RAIN]: $CityName ($Location) is Bad ($rain_3hrs mm/3hrs)"
         $error_reason = "RAIN: Failed because of rain $rain_3hrs mm/3Hrs $Location ($CityName)"
         $bad++
     }
-    write-host ""
+    Write-Verbose "[Progress] Good: $good, Sketchy: $Sketchy, Bad: $bad"
+    write-Verbose ""
 
 } #Foreach
 
@@ -115,21 +121,14 @@ $number_of_locations = (($locations | Measure-Object).Count) * 3
 $bad = [math]::Round(($bad / $number_of_locations) * 100, 0)
 $sketchy = [math]::Round(( $sketchy / $number_of_locations * 100), 0)
 $good = [math]::Round(($good / $number_of_locations * 100), 0)
-if ($verbose = 'yes') { write-host "$good% were good. $sketchy% were sketchy. $bad% were bad." }   
-#if ($error_reason) { write-host "$error_reason is why we're failing this" }
+Write-Verbose "[Summary] Locations: $number_of_locations, Good: $good, Sketchy: $Sketchy, Bad: $bad"
+
 if ($bad) {
-    write-host "Failing because of Bad Weather"
-    write-host "$error_reason"
-    exit 1
-} 
-
-#Wind Logic
-# <x eveywhere
-# what direction am I headed?
-
-#Rain Logic
-# <x mm everywhere
-# >x, <y, sketchy++;
-
-#Overall Y/N
-# - Sketchyness <2
+    Write-Verbose "Failing because of Bad Weather"
+    Write-Verbose "$error_reason"
+    return $false
+}
+else {
+    return $true
+}
+write-verbose "$good`% were good. $sketchy`% were sketchy. $bad`% were bad."
